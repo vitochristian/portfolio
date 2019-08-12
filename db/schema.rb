ace017ab5f0b1fa0c9881684ff01f8aaae05ed8c
Rails Udemy Course <rails@gmail.com>
2019-08-12 10:46:24 +0700

    Updated status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_12_025626) do
+ActiveRecord::Schema.define(version: 2019_08_12_034001) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_12_025626) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
