35496d0265f199a9a46dc6ddc97887cc06d02833
Rails Udemy Course <rails@gmail.com>
2019-08-13 08:22:02 +0700

    Integrated database relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+
   root to: 'pages#home'
+  
   get 'about-us', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  resources :blogs
+  
+  resources :blogs do
+  	member do
+  		get :toggle_stats
+  	end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
