12925c3384dd4bd15ab0385a5adaf36fc04d63a7
Rails Udemy Course <rails@gmail.com>
2019-08-13 08:39:26 +0700

    Implemented custom scopes for portfolio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+	)
+end
+
 10.times do |blog|
 	Blog.create!(
 		title: "My blog post #{blog}",
@@ -12,7 +18,8 @@
 		nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
 		ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea
 		voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem
-		eum fugiat quo voluptas nulla pariatur?"
+		eum fugiat quo voluptas nulla pariatur?",
+		topic_id: Topic.last.id
 	)
 end
 
@@ -23,10 +30,10 @@ end
 	)
 end
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
 		title: "Portfolio title: #{portfolio_item}",
-		subtitle: "My services",
+		subtitle: "Ruby on Rails",
 		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 		eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
 		veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
@@ -37,5 +44,20 @@ end
 		main_image: "http://placehold.it/600x400",
 		thumb_image: "http://placehold.it/300x200" 
 
+	)
+end
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "Angular",
+		body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+		eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+		veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+		commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+		esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+		cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+		est laborum.",
+		main_image: "http://placehold.it/600x400",
+		thumb_image: "http://placehold.it/300x200" 
 	)
 end
