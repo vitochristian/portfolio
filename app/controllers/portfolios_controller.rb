bfc261ded08826cda7259f6befb6bf2adbbdf9a2
Rails Udemy Course <rails@gmail.com>
2019-08-13 10:47:24 +0700

    Implemented form config for attr'

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,11 +9,11 @@ class PortfoliosController < ApplicationController
 
 	def new
 		@portfolio_item = Portfolio.new
+		2.times { @portfolio_item.technologies.build }
 	end
 
 	def create
-		@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
-
+		@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body, technologies_attributes: [:name]))
 	    respond_to do |format|
 	      if @portfolio_item.save
 	        format.html { redirect_to portfolios_path, notice: 'Portfolio item is now live' }
