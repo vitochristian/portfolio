befa52b3cd8ac064f7b5e21622484c60e6d908dd
Rails Udemy Course <rails@gmail.com>
2019-08-14 14:52:53 +0700

    Added concern controller for devise whitelist

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-	before_action :configure_permitted_params, if: :devise_controller?
-
-	def configure_permitted_params
-		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-	end
+	include DeviseWhitelist
 end
