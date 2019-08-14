84606c360b0b6d5814cfbbc02193ea55351e1f06
Rails Udemy Course <rails@gmail.com>
2019-08-14 16:06:53 +0700

    Added session tracker for dynamic link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
 	include DeviseWhitelist
+	before_action :set_source
+	def set_source
+		session[:source] = params[:q] if params[:q]
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates_presence_of :name
+  
   def first_name
   	self.name.split.first
   end
