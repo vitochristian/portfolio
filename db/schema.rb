21f500a404f47284efd9a7d50a5e59b2c4e2c2c8
Rails Udemy Course <rails@gmail.com>
2019-09-03 23:25:18 +0700

    Edit installation req for petergate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_13_091217) do
+ActiveRecord::Schema.define(version: 2019_09_03_162321) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_08_13_091217) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "roles"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
